364c0660bf79f2352cc6d27a4af2ebf31225274e
tristan01-jpg <>
2026-07-07 10:31:38 +0000

    For loops

---
+++ loops/for_loops/main.py
@@ -0,0 +1,11 @@
+prices = [12.99, 8.50, 15.75, 23.00, 7.25]
+
+# Write your code here
+#1. Create a total
+total = 0
+for price in prices:
+    total += price 
+
+#2. Print the final total
+print("Total price for products:", total)
+
