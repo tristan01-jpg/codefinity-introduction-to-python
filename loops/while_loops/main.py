8d494f7963b25bab8b2c78086ed2ef1f858738af
tristan01-jpg <>
2026-07-07 10:48:09 +0000

    While Loops

---
+++ loops/while_loops/main.py
@@ -0,0 +1,13 @@
+start_number = 5
+countdown_values = []
+
+#1. Create a new variable equal to start_number
+current_number = start_number
+#2. Create a while loop 
+while current_number > 0:
+    countdown_values.append(current_number)
+    current_number -= 1
+
+print("Dsicount countdown complete!")
+print(countdown_values)
+    
